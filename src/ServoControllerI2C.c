41800466cec3a87506a1ab3807081d8a5270c556
Pengu MC @ vito <michiel.van.der.coelen@gmail.com>
2014-09-07 19:59:21 +0200

    single pin working
    1.0833 us resolution (yes, microseconds)

--- src/ServoControllerI2C.c
+++ src/ServoControllerI2C.c
@@ -47,10 +47,13 @@
 
 #define SET(x,y) (x|=(1<<y))
 #define CLR(x,y) (x&=(~(1<<y)))
-#define CHK(x,y) (x&(1<<y)) 
+#define CHK(x,y) (x&(1<<y))
 #define TOG(x,y) (x^=(1<<y))
 
 #define SERVO_AMOUNT 12
+
+#define ASM1700LO 0x22
+#define ASM1700HI 0x06
 
 typedef struct {
   uint16_t stop;
@@ -77,34 +80,39 @@ void init_channels() {
   }
   DDRA = 0xFF;
   DDRC |= (1<<PC2)|(1<<PC3)|(1<<PC4)|(1<<PC5);
-  servo_channels[0].stop = 0x85ec;
+
+  servo_channels[0].port = _SFR_IO_ADDR(PORTD);
+  servo_channels[0].pin = 5;
+  servo_channels[0].stop =((ASM1700HI<<8)|(ASM1700LO)) - 1015;
+  // base value - (x/T_clk) / 13
+  // where x is the time you want added to 400us
 }
 
 // -----------------------------------------------------------------------------
 //                                                                   init_timers
 // -----------------------------------------------------------------------------
-// void init_timers() {
+void init_timers() {
   // using timer 0 (8 bit) with 1024 prescaler in CTC mode
   // to get about 50 Hz
-  // TCCR0 = (1<<WGM01)|(1<<CS02)|(1<<CS00);
-  // OCR0 = 234;  // (0.02 / (1/(12e6/1024)) = 234.375
-  // SET(TIMSK, OCIE0);
-  // sei();
-// }
+  TCCR0 = (1<<WGM01)|(1<<CS02)|(1<<CS00);
+  OCR0 = 234u;  // (0.02 / (1/(12e6/1024)) = 234.375
+  SET(TIMSK, OCIE0);
+  sei();
+}
 
 // -----------------------------------------------------------------------------
 //                                                                          main
 // -----------------------------------------------------------------------------
 int main() {
-  wdt_enable(WDTO_1S);
   init_channels();
-  DDRD = (1<<PD5)|(1<<PD3);
-  PORTD = (1<<PD5)|(0<<PD3); // red , green
-
+  DDRD = 0xff;//(1<<PD5)|(1<<PD3);
+  //PORTD = (1<<PD5)|(0<<PD3); // red , green
+  init_timers();
+  
   /*
   - set all ddr
   - start timer for 50 Hz
-  
+
   - 50 hz ISR
   - enter assembly
   - cli
@@ -112,49 +120,98 @@ int main() {
   - wait 400 us
   - set counter to 1700 us
   - # loop
-  - check each pin counter, set to 0 if needed counters for each pin need to be 
+  - check each pin counter, set to 0 if needed counters for each pin need to be
     offset individually. make sure setting and doing nothing takes the same
     amount of cycles
-    
+
     cut into groups to reduce looptime? can be done after first implementation
-    
+
   - decrement counter
   - if 0 exit assembly
   - else jump to loop
   - exit assembly
   - sei
   - exit ISR
-  
+
   - main loop:
   - handle I2C
   - clear wdt
-  
+
   */
-   
-   // seperate 2x8 bits works, 1x16 doesn't? try making it volatile
-   
-  __asm__("CLI \n\t"
-          "LDI r23, 0x20 \n\t" // r23: PD5 high
-          "EOR r22, r22 \n\t" // clear r22
-          "start: \n\t"
-          "LD r24, %a0 \n\t"   // 2
-          "LDD r25, %a0+1 \n\t"// 2
-          "EOR r22, r23 \n\t"  // 1 toggle bit 5
-          "OUT 0x12, r22 \n\t" // 1 put on portd
-          "loopje: \n\t"
-          "WDR \n\t"            // 1 reset watchdog
-          "SBIW r24, 0x01 \n\t" // 2
-          "BRCS start \n\t"     // 1 (2 on true) if carry set, jump to start
-          "JMP loopje \n\t"     // 3
-  ::"b"(&servo_channels[0])); // loopcycles = 7, exitcycles = 11
-  
+  wdt_enable(WDTO_1S);
+  while(1) {
+    wdt_reset();
+  }
 }
 
 // -----------------------------------------------------------------------------
 //                                                                       TIMER 0
 // -----------------------------------------------------------------------------
-// ISR(TIMER0_COMP_vect){
-  // __asm__("CLI \n\t"
-          // "SEI \n\t"
-  // :::"r24"); // start point to first channel, size of a channel, 
-// }
+ISR(TIMER0_COMP_vect){
+  // 400e-6 / (T_clk *5) = 960 = 0x3c0
+  // 2100e-6 / (T_clk *5) = 5039.9 = 0x13b0
+  
+  /*
+  counter == stop
+    3 first cp skips 
+    1 second cp
+    1 brne, but r16 = r24 so don't branch
+    1 clear pin
+    3 jump somewhere
+   ---
+    9
+  
+  counter != stop, r17 = r25 but r16 != r24
+    3 first cp skips
+    1 cp
+    2 brne true, so branch to noclear2
+   --- 
+    6 still needs 3 nops
+  
+  count  != stop, r17 != r25
+   1 fist cp doesn't skip
+   3 jump to noclear1
+  ---
+   4 still needs 5 nops
+   
+   total loopcycles = 13 (2 for sbiw, 2 for branch)
+  */
+  TOG(PORTD, PD3);
+  __asm__(
+          "CLI \n\t"
+          "SBI 0x12, 5 \n\t"
+          // wait 400 us
+          "LDI r24, %1 \n\t"  // 1
+          "LDI r25, %0 \n\t"  // 2
+          "loopje: \n\t"  
+          "WDR \n\t"
+          "SBIW r24, 0x01 \n\t" // 2
+          "BRNE loopje \n\t"  // 1 (2 on true)
+          // load counter value for 1700 us
+          "LDI r24, %3 \n\t"
+          "LDI r25, %2 \n\t"
+          "LD r16, %a4 \n\t"   // 1
+          "LDD r17, %a4+1 \n\t"// 1
+          "checkloop: \n\t" 
+          "CPSE r17, r25 \n\t" // 1 (3 on equal since jmp is 2 words)
+          "JMP noclear1 \n\t"  // 3
+          "CP r16, r24 \n\t"
+          "BRNE noclear2 \n\t" //
+          "CBI 0x12, 5 \n\t"   // 1
+          "JMP finalpart \n\t"
+          "noclear1: \n\t"
+          "nop \n\t"  // 1
+          "nop \n\t"  // 1
+          "noclear2: \n\t"
+          "nop \n\t"  // 1
+          "nop \n\t"  // 1
+          "nop \n\t"  // 1
+          "finalpart: \n\t"
+          "SBIW r24, 0x01 \n\t" // 2
+          "BRNE checkloop \n\t" // 1 (2 on true)
+          
+          "CBI 0x12, 5 \n\t" 
+          "SEI \n\t"
+  ::"M"(0x03),"M"(0xc0),"M"(0x06),"M"(0x22),"e"(&servo_channels[0]));
+  
+}
